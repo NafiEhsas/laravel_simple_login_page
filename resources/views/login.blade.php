82fff1ff4203bc7a38bf249a28b7b8687daeec17
NafiEhsas <nafiehsas77@gmail.com>
2026-09-23 08:42:18 +0430

    updated route

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Login</title>
-      @vite('resources/css/login.css')
+      <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
     <div class="login-container">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
